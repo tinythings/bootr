6a91b7d5bb2ecbd8bca856dbdfc0f9fe2016d688
Bo Maryniuk <bo@maryniuk.net>
2024-06-25 19:22:59 +0000

    Implement application of additional diff layers

--- src/ociman/installer.rs
+++ src/ociman/installer.rs
@@ -2,12 +2,15 @@ use crate::bconf::defaults;
 use dircpy::CopyBuilder;
 use flate2::read::GzDecoder;
 use log::{debug, error, info, warn};
+use nix::NixPath;
 use std::{
+    collections::HashMap,
     fs::{self, File},
     io::Error,
     path::{Path, PathBuf},
 };
 use tar::Archive;
+use walkdir::WalkDir;
 
 use super::ocistate::{OCIMeta, OCIMetaTryFrom};
 
@@ -54,6 +57,8 @@ pub struct OCIInstaller {
 
 /// Implementation of the OCI container installer.
 impl OCIInstaller {
+    pub const WHITEOUT: &'static str = ".wh.";
+
     /// Constructor
     pub fn new<P: AsRef<Path>>(pth: P) -> Self {
         OCIInstaller {
@@ -63,24 +68,103 @@ impl OCIInstaller {
         }
     }
 
+    /// Apply a layer to the root filesystem.
+    ///
+    /// This works the following way:
+    /// - If this is a very first layer (installation mode, directory is empty),
+    ///   it is simply unpacked "as is"
+    ///
+    /// - Every other layer (diff) is applied into its own sub-directory,
+    ///   named after its hash, and then copied over the final target.
+    ///   White-outs are resolved to the artifacts on the target and deleted,
+    ///   and then markers are removed from the layer. Once this done, layer
+    ///   is simply copied over.
+    ///
+    fn apply_layer(&self, is_base: bool, layer_arc_pth: &PathBuf, digest: String) -> Result<(), Error> {
+        debug!("Unpacking archive to {:?}", self.buildroot);
+        if is_base {
+            // First time install, just unpack without any post-processing
+            Archive::new(GzDecoder::new(File::open(layer_arc_pth)?)).unpack(&self.buildroot)?;
+            fs::remove_file(layer_arc_pth)?;
+            info!("Base layer {} applied", digest);
+        } else {
+            let dst = self.buildroot.join("tmp"); // This is a /tmp on the target, but might not exist
+            let dst_dirty = !dst.exists();
+            if dst_dirty {
+                debug!("Creating temp directory on target at {:?}", dst);
+                fs::create_dir_all(&dst)?;
+            }
+            let dst = dst.join(digest.as_str()); // This contains /tmp/<SHA256> with the diff in it
+            fs::create_dir_all(&dst)?;
+
+            Archive::new(GzDecoder::new(File::open(layer_arc_pth)?)).unpack(&dst)?;
+            fs::remove_file(layer_arc_pth)?;
+
+            for e in WalkDir::new(&dst).into_iter().flatten() {
+                if e.file_name().to_str().unwrap().starts_with(Self::WHITEOUT) {
+                    // Trim the temporary path to diff-related path only
+                    let wh_p = self.buildroot.join(
+                        e.path()
+                            .to_str()
+                            .unwrap()
+                            .split_once(&digest)
+                            .unwrap_or_default()
+                            .1
+                            .trim_start_matches('/')
+                            .replace(Self::WHITEOUT, ""),
+                    );
+
+                    // If diff went bonkers, we just stop everything, preventing permanent system damage
+                    if !wh_p.exists() {
+                        return Err(Error::new(
+                            std::io::ErrorKind::NotFound,
+                            format!("Requested object does not exists at {:?}", wh_p),
+                        ));
+                    }
+
+                    // First, remove the target
+                    if wh_p.is_dir() {
+                        fs::remove_dir_all(&wh_p)?;
+                    } else {
+                        fs::remove_file(&wh_p)?;
+                    }
+
+                    // Now remove the whiteout marker
+                    fs::remove_file(e.path())?;
+                }
+            }
+
+            // Copy over the temporary diff and remove the temp
+            CopyBuilder::new(&dst, &self.buildroot).overwrite(true).run()?;
+
+            // Remove temporary directory for this layer
+            fs::remove_dir_all(&dst)?;
+            if dst_dirty {
+                fs::remove_dir_all(dst.parent().unwrap())?;
+            }
+
+            info!("Diff layer {} applied", digest);
+        }
+        Ok(())
+    }
+
     /// Unpack required layers and remove them from the disk, once done.
     fn populate_oci_data(&self) -> Result<(), Error> {
         info!("Applying OCI data");
         let br_parent = self.buildroot.parent().unwrap().to_path_buf();
         let meta = <OCIMeta as OCIMetaTryFrom<PathBuf>>::try_from(br_parent.join(defaults::C_BOOTR_SECT_OCI_META).to_owned())?;
-        for layer in meta.layers {
+        for (idx, layer) in meta.layers.iter().enumerate() {
             // Handle "vnd.docker.image" and "vnd.oci.image". What a mess... :-(
             if layer.media_type.ends_with("tar.gzip") || layer.media_type.ends_with(".tar+gzip") {
-                let arcp = &br_parent.join(layer.digest.trim_start_matches("sha256:"));
+                let digest = layer.digest.trim_start_matches("sha256:").to_owned();
+                let arcp = &br_parent.join(&digest);
                 debug!("Reading layer at {:?}", arcp);
                 if !arcp.exists() {
                     return Err(Error::new(std::io::ErrorKind::NotFound, format!("Layer file at {:?} was not found", arcp)));
                 }
 
-                debug!("Unpacking archive to {:?}", self.buildroot);
-                Archive::new(GzDecoder::new(File::open(arcp)?)).unpack(&self.buildroot)?;
-                fs::remove_file(arcp)?;
-                info!("Layer {} applied", layer.digest);
+                // XXX: applied layers needs to be skipped when updating!
+                self.apply_layer(idx == 0, arcp, digest)?;
             }
         }
         Ok(())
@@ -146,7 +230,7 @@ impl OCIInstaller {
         // If kernel requested to be preserved, keep it.
         // This then include /boot as well, because the initramfs
         // won't be regenerated at this point.
-        self.maybe_keep_kernel()?;
+        //self.maybe_keep_kernel()?;
 
         Ok(())
     }
